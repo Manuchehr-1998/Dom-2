05fd838cdfb99ad83d3f7b6a95c83011efc0ee26
Manuchehr-1998 <120194005+Manuchehr-1998@users.noreply.github.com>
2023-01-13 15:10:53 +0500

    sssss

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let btn = document.createElement("button");
+btn.innerHTML = "Hello Button";
+document.body.appendChild(btn);
+// lampuchka
+// let on = "https://www.w3schools.com/js/pic_bulboff.gif";
+// let off = "https://www.w3schools.com/js/pic_bulbon.gif";
+// let btn = document.querySelector(".btn");
+// let img = document.querySelector(".img");
+
+// btn.onclick = () => {
+//   if (btn.innerText === "on") {
+//     btn.innerHTML = "off";
+//     img.src = on;
+//   } else {
+//     btn.innerHTML = "on";
+//     img.src = off;
+//   }
+// };
